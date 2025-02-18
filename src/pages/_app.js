01827b49a76dfa2d59b359ae26d98c961017cece
DariyaSav <dsavinova85@gmail.com>
2025-02-18 10:15:34 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,63 @@
 import '../css/main.css';
+
+// Changing fonts
+
+import { Inter, Tinos } from 'next/font/google';
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const tinos = Tinos({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+import { tinos } from './fonts';
+ 
+export default function Layout({ children }) {
+  return (
+    <html lang="en" className={tinos.className}>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
+import { inter } from './fonts';
+ 
+export default function Page() {
+  return (
+    <>
+      <h1 className={inter.className}>My page</h1>
+      <h2 className={inter.className}>My page</h2>
+      <h3 className={inter.className}>My page</h3>
+      <h4 className={inter.className}>My page</h4>
+      <h5 className={inter.className}>My page</h5>
+      <h6 className={inter.className}>My page</h6>
+    </>
+  )
+}
+
+
+
+
+ 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
+
+
+
+
+
+
+
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import ReactGA from 'react-ga4';
